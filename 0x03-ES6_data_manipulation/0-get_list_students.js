5548332e53210e910d1c48b72305f3eb7da5fa9c
snum01 <johnjim4life@gmail.com>
2023-07-03 17:46:25 +0000

    task 0

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,8 +1,7 @@
-const getListStudentIds = (myArray) => {
-  if (!Array.isArray(myArray)) {
-    return [];
-  }
-  return myArray.map((studentId) => studentId.id);
-};
-
-export default getListStudentIds;
+export default function getListStudents() {
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+}
